5e9e2b2f31ae7a25a50aa1f4f27fe54283bf7897
Durhamfighter <sinseunghun708@gmail.com>
2025-01-11 07:26:15 +0000

    Refactor: add sensitivity function in utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np
 
 
@@ -10,3 +11,15 @@ def set_seed(seed=42):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+def sensitivity_check(model):
+    sensitivity_layer={}
+    for name,module in model.named_modules():
+        if isinstance(module,nn.Conv2d) and 'downsample' not in name:
+            L1_weight= module.weight.data.cpu().numpy()
+            L1_weight=L1_weight.reshape(L1_weight.shape[0],-1)
+            L1_weight=np.sort(np.sum(np.abs(L1_weight),axis=1))[::-1]
+            L1_weight=L1_weight/L1_weight[0]
+            #L2_weight = torch.sqrt(torch.sum(module.weight,dim=(1,2,3)))
+            sensitivity_layer[name]=L1_weight
+    return sensitivity_layer
